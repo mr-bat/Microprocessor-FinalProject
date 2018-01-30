ed5d20420bd68bd9adf2778132536f941d3fc273
Abtin Bateni <abtinbateni@gmail.com>
2018-01-30 16:13:55 +0330

    added functions to code and made it ready to compile&upload.

--- micro1.c
+++ micro1.c
@@ -1,6 +1,212 @@
+/*****************************************************
+This program was produced by the
+CodeWizardAVR V2.05.0 Advanced
+Automatic Program Generator
+� Copyright 1998-2010 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project :
+Version :
+Date    : 1/30/2018
+Author  : www.Eca.ir *** www.Webkade.ir
+Company :
+Comments:
+
+
+Chip type               : ATmega32A
+Program type            : Application
+AVR Core Clock frequency: 8.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 512
+*****************************************************/
+
+#include <mega32a.h>
+
+// Alphanumeric LCD Module functions
+#include <alcd.h>
+#include <delay.h>
+
+#ifndef RXB8
+#define RXB8 1
+#endif
+
+#ifndef TXB8
+#define TXB8 0
+#endif
+
+#ifndef UPE
+#define UPE 2
+#endif
+
+#ifndef DOR
+#define DOR 3
+#endif
+
+#ifndef FE
+#define FE 4
+#endif
+
+#ifndef UDRE
+#define UDRE 5
+#endif
+
+#ifndef RXC
+#define RXC 7
+#endif
+
+#define FRAMING_ERROR (1<<FE)
+#define PARITY_ERROR (1<<UPE)
+#define DATA_OVERRUN (1<<DOR)
+#define DATA_REGISTER_EMPTY (1<<UDRE)
+#define RX_COMPLETE (1<<RXC)
+
+// USART Receiver buffer
+#define RX_BUFFER_SIZE 120
+char rx_buffer[RX_BUFFER_SIZE];
+unsigned char gps_init[] = {"AT+CGPSPWR=1"};
+unsigned char gps_status[] = {"AT+CGPSSTATUS?"};
+unsigned char gps_getloc[] = {"AT+CGPSOUT=32"};
+unsigned char loc[100] = {""};
+int writeLoc, locEndInd;
+
+#if RX_BUFFER_SIZE <= 256
+unsigned char rx_wr_index,rx_rd_index,rx_counter;
+#else
+unsigned int rx_wr_index,rx_rd_index,rx_counter;
+#endif
+
+// This flag is set on USART Receiver buffer overflow
+bit rx_buffer_overflow;
+
+// USART Receiver interrupt service routine
+interrupt [USART_RXC] void usart_rx_isr(void)
+{
+char status,data;
+status=UCSRA;
+data=UDR;
+if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
+   {
+   rx_buffer[rx_wr_index++]=data;
+#if RX_BUFFER_SIZE == 256
+   // special case for receiver buffer size=256
+   if (++rx_counter == 0)
+      {
+#else
+   if (rx_wr_index == RX_BUFFER_SIZE) rx_wr_index=0;
+   if (++rx_counter == RX_BUFFER_SIZE)
+      {
+      rx_counter=0;
+#endif
+      rx_buffer_overflow=1;
+      }
+   }
+   lcd_putchar(data);
+   if(writeLoc == 1)
+      loc[locEndInd++] = data;
+}
+
+#ifndef _DEBUG_TERMINAL_IO_
+// Get a character from the USART Receiver buffer
+#define _ALTERNATE_GETCHAR_
+#pragma used+
+char getchar(void)
+{
+char data;
+while (rx_counter==0);
+data=rx_buffer[rx_rd_index++];
+#if RX_BUFFER_SIZE != 256
+if (rx_rd_index == RX_BUFFER_SIZE) rx_rd_index=0;
+#endif
+#asm("cli")
+--rx_counter;
+#asm("sei")
+return data;
+}
+#pragma used-
+#endif
+
+// USART Transmitter buffer
+#define TX_BUFFER_SIZE 120
+char tx_buffer[TX_BUFFER_SIZE];
+
+#if TX_BUFFER_SIZE <= 256
+unsigned char tx_wr_index,tx_rd_index,tx_counter;
+#else
+unsigned int tx_wr_index,tx_rd_index,tx_counter;
+#endif
+
+// USART Transmitter interrupt service routine
+interrupt [USART_TXC] void usart_tx_isr(void)
+{
+if (tx_counter)
+   {
+   --tx_counter;
+   UDR=tx_buffer[tx_rd_index++];
+#if TX_BUFFER_SIZE != 256
+   if (tx_rd_index == TX_BUFFER_SIZE) tx_rd_index=0;
+#endif
+   }
+}
+
+#ifndef _DEBUG_TERMINAL_IO_
+// Write a character to the USART Transmitter buffer
+#define _ALTERNATE_PUTCHAR_
+#pragma used+
+void putchar(char c)
+{
+while (tx_counter == TX_BUFFER_SIZE);
+#asm("cli")
+if (tx_counter || ((UCSRA & DATA_REGISTER_EMPTY)==0))
+   {
+   tx_buffer[tx_wr_index++]=c;
+#if TX_BUFFER_SIZE != 256
+   if (tx_wr_index == TX_BUFFER_SIZE) tx_wr_index=0;
+#endif
+   ++tx_counter;
+   }
+else
+   UDR=c;
+#asm("sei")
+}
+#pragma used-
+#endif
+
+// Standard Input/Output functions
+#include <stdio.h>
+
+// Declare your global variables here
+
+void usartinit()
+{
+    UBRRH=00;
+    UBRRL=77;
+    UCSRB|=(1<<RXEN)|(1<<TXEN);
+    UCSRC|=(1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);
+}
+
+void put_command(unsigned char *s) {
+    int i;
+    for( i = 0; s[i]; ++i)
+        putchar(s[i]);
+    putchar('\r');
+}
+
+void init() {
+    unsigned char s1[] = {"AT"};
+    put_command(s1);
+}
+
+void init_gps() {
+    put_command(gps_init);
+    put_command(gps_status);
+    put_command(gps_getloc);
+}
+
 void sendLoc() {
-  for (int i = 0; i < end; i++) {
-    PORTB = Loc[i];
+  int i;
+  for (i = 0; i < locEndInd; i++) {
+    PORTB = loc[i];
     PORTD.4 = 1;
     delay_ms(1);
     PORTD.4 = 0;
@@ -13,7 +219,163 @@ void sendLoc() {
   PORTD.4 = 0;
 }
 
-void ISR() {
-  while(PORTD.3 == 1) ;
-  sendLoc();
+
+// External Interrupt 1 service routine
+interrupt [EXT_INT1] void ext_int1_isr(void)
+{
+    while(PORTD.3 == 1) ;
+    sendLoc();
+}
+
+void main(void)
+{
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T
+PORTA=0x00;
+DDRA=0x00;
+
+// Port B initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T
+PORTB=0x00;
+DDRB=0x00;
+
+// Port C initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T
+PORTC=0x00;
+DDRC=0x00;
+
+// Port D initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T
+PORTD=0x00;
+DDRD=0x00;
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=0xFF
+// OC0 output: Disconnected
+TCCR0=0x00;
+TCNT0=0x00;
+OCR0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: Timer1 Stopped
+// Mode: Normal top=0xFFFF
+// OC1A output: Discon.
+// OC1B output: Discon.
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=0x00;
+TCCR1B=0x00;
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0x00;
+TCCR2=0x00;
+TCNT2=0x00;
+OCR2=0x00;
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: Off
+MCUCR=0x00;
+MCUCSR=0x00;
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: On
+// INT1 Mode: Low level
+// INT2: Off
+GICR|=0x80;
+MCUCR=0x00;
+MCUCSR=0x00;
+GIFR=0x80;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=0x00;
+
+// USART initialization
+// Communication Parameters: 8 Data, 1 Stop, No Parity
+// USART Receiver: On
+// USART Transmitter: On
+// USART Mode: Asynchronous
+// USART Baud Rate: 9600 (Double Speed Mode)
+UCSRA=0x02;
+UCSRB=0xD8;
+UCSRC=0x06;
+UBRRH=0x00;
+UBRRL=0x67;
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// Analog Comparator Input Capture by Timer/Counter 1: Off
+ACSR=0x80;
+SFIOR=0x00;
+
+// ADC initialization
+// ADC disabled
+ADCSRA=0x00;
+
+// SPI initialization
+// SPI disabled
+SPCR=0x00;
+
+// TWI initialization
+// TWI disabled
+TWCR=0x00;
+
+
+// Alphanumeric LCD initialization
+// Connections specified in the
+// Project|Configure|C Compiler|Libraries|Alphanumeric LCD menu:
+// RS - PORTA Bit 0
+// RD - PORTA Bit 1
+// EN - PORTA Bit 2
+// D4 - PORTA Bit 4
+// D5 - PORTA Bit 5
+// D6 - PORTA Bit 6
+// D7 - PORTA Bit 7
+// Characters/line: 16
+lcd_init(16);
+
+// Global enable interrupts
+#asm("sei")
+
+usartinit(); //what is it?
+init();
+init_gps();
+while (1)
+      {
+            // Place your code here
+            delay_ms(1000);
+            put_command(gps_status);
+            writeLoc = 1;
+            locEndInd = 0;
+            put_command(gps_getloc);
+            writeLoc = 0;
+      }
 }
